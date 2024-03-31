fe599549c48680158aaa95130041b29fd03aaadb
pyovski <dev.natalyalves@gmail.com>
2024-03-30 23:07:35 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+Console.Write("Digite um número inteiro positivo para a tabuada: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+
+for(int contador = 0; contador <= 10; contador++ ){
+
+    Console.WriteLine($"{numero} x {contador} = {numero * contador}" );
+}
